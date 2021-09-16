ec9559972ab0e899f0f4992e80fe7da7606a1d7a
msowa <msowa@live.co.uk>
2021-09-16 14:09:22 +0100

    day2update

--- catapp/src/components/CatPage.js
+++ catapp/src/components/CatPage.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
 const CatPage = () => {
-  return <h1>Cat Page</h1>
+  return (
+    <>
+      <div className="grid-container">
+        <div className="grid-item">
+          <h1>Cat Breed</h1>
+        </div>
+        <div className="grid-item">
+          <h2>Cat Origin</h2>
+        </div>
+        <div className="grid-item">
+          <p>Cat image</p>
+        </div>
+      </div>
+      <div className="grid-item">
+        <p>Cat Description</p>
+      </div>
+    </>
+  )
 }
 
 export default CatPage
